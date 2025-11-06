73713ad06bcc6b4845907f15ef6db6c87a13c2f9
supersheykh <bahsekousidi@gmail.com>
2025-11-06 19:11:37 +0000

    now let's see

--- actions/users/get-all.ts
+++ actions/users/get-all.ts
@@ -1,7 +1,7 @@
 import { createServerFn } from "@tanstack/react-start";
 import { user } from "auth-db/auth-schema";
-import db from "@/db";
+import db from "auth-db/index";
 
 export const getAllUsers = createServerFn().handler(async () => {
-  return db.select().from(user).all();
+  return db.select().from(user);
 });
